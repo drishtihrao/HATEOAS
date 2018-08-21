b9c4c026af3511dfdc3eb6063ea73de109cd579b
drishtihrao <drishtihrao@gmail.com>
2018-08-21 07:33:04 -0400

    HATEOAS

---
+++ src/main/java/com/demo/rest/service/Service.java
@@ -0,0 +1,12 @@
+package com.demo.rest.service;
+
+import java.util.Collection;
+
+import com.demo.rest.pojo.Customer;
+
+public interface Service {
+	void addCustomer(Customer customer);
+	Collection<Customer> viewAllCustomers();
+	void updateCustomer(Customer customer);
+	void deleteCustomer(int customerId);
+}
---
+++ src/main/java/com/demo/rest/test/Demo.java
@@ -0,0 +1,75 @@
+package com.demo.rest.test;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.rest.pojo.Customer;
+import com.demo.rest.service.ServiceImpl;
+
+@RestController
+public class Demo {
+
+	@Autowired
+	private ServiceImpl service; // = new ServiceImpl();
+
+	@RequestMapping(value = "/customer/add", method = RequestMethod.POST/*, consumes = "application/json"*/)
+	public void addCustomer(@RequestBody Customer customer) {
+		System.out.println(customer);
+		service.addCustomer(customer);
+	}
+
+	@RequestMapping(value = "/customers", method = RequestMethod.GET/*, produces = MediaType.ALL_VALUE*/)
+	public Collection<Customer> viewAllCustomers() {
+		for(Customer customer:service.viewAllCustomers()){ 
+			
+			Link link= ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).deleteCustomer(customer.getCustomerId())).withSelfRel();
+			customer.add(link);
+			/*break;*/
+		}
+		
+		return service.viewAllCustomers();
+	}
+
+	@RequestMapping(value = "/customer/update", method = RequestMethod.PUT/*, consumes = "application/json"*/)
+	public void updateCustomer(int id, @RequestBody Customer customer) {
+		System.out.println(customer);
+		service.updateCustomer(customer);
+	}
+
+	@RequestMapping(value = "/customer/delete", method = RequestMethod.DELETE)
+	public ResponseEntity<String> deleteCustomer(@RequestParam(value="customerId") int customerId) {
+		service.deleteCustomer(customerId);
+		return new ResponseEntity<String>("Entity Deleted", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/customer/searchById", method = RequestMethod.GET)
+	public Resource<Customer> searchById(int customerId){
+		Resource<Customer> resource = null;
+		for(Customer customer: service.viewAllCustomers()) {
+			if(customer.getCustomerId() == customerId) {
+				resource = new Resource<Customer>(customer);
+				break;
+			}
+		}
+		
+		//Link link = new Link("http://localhost:8080/customers");
+		//ControllerLinkBuilder link = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).viewAllCustomers());
+		//resource.add(link.withRel("All Customers"));
+		resource.add(ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).deleteCustomer(customerId)).withSelfRel());
+		
+		return resource;
+	}
+	
+
+}
